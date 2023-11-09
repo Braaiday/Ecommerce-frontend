a8aa55f3c47f9453a0d3dc547c52d9ef3d35a10a
Braaiday <pierre.putter@gmail.com>
2023-11-09 18:01:31 +0200

    Cleanup

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import store from './store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-import './style/_main.scss';
 import { AuthProvider } from './utils/AuthProvider';
+import './style/_main.scss';
 
 if (process.env.NODE_ENV === 'production') disableReactDevTools();
 const root = ReactDOM.createRoot(document.getElementById('root'));
